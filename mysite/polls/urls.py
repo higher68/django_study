29d382e897104ff27ae11eb56ebdf567db33900d
junya <hatiro7code@gmail.com>
2018-08-18 15:10:20 +0900

    20180818-path-understood

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,12 +3,14 @@
 from django.urls import path
 
 from . import views
-
+# path()
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
+    # 第一引数が、urlの情報、第二引数が、左の条件を満たす時に、参照するパス
+    # 第３引数が、上記パターンの名前
     # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail',
+    path('<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
     path('<int:question_id./results/', views.results,
     name = 'results'),
