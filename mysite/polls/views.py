cd218fae2f666f22936cd1381f2446feada3086d
junya <hatiro7code@gmail.com>
2018-08-20 23:56:27 +0900

    20180820 viewsi-render

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,16 +3,24 @@ from django.http import HttpResponse
 # %の意味について文字列中の%sの部分が、後ろに与えられた引数で置換される。
 # viewの役割・・・１：リクエストされたページのコンテンツを含むオブジェクトを返す
 # ２：Http404のような例外の送出
-from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+
+from .models import Question
 
 def index(request):
     # 並べ替え
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     # modelclass.modelmanager.method()という形でクエリを実行
-    output = ', '.join([q.question_text for q in late_question_list])
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list':latest_question_list}
+    output = ', '.join([q.question_text for q in latest_question_list])
     # 区切り文字.join(list)として、区切り文字を入れて連結できる
     # システム上の最新5件の質問をカンマで区切り、日付順に表示
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # rendering表示用のデータを元に、内容を整形して表示すること
+    # マッピング：対応づけること？
+    # テンプレートを指定のコンテキストでレンダリングし、そのHttpResponseオブジェクトを返す。
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
